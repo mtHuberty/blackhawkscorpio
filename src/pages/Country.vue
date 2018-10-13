663f2c0734c86653576fb4612290f6aea96b763e
Chris Marfia <chris.marfia@monsanto.com>
2018-10-13 09:33:58 -0500

    Added all the country flags.

--- src/pages/Country.vue
+++ src/pages/Country.vue
@@ -4,8 +4,8 @@
       <v-card>
         <v-container grid-list-sm fluid>
           <v-layout row wrap>
-            <v-flex v-for="n in 9" :key="n" xs6 sm4 m2 d-flex>
-              <Flag country="us"></Flag>
+            <v-flex v-for="country in countries" :key="country" xs6 sm4 m2 d-flex>
+              <Flag :country="country"></Flag>
             </v-flex>
           </v-layout>
         </v-container>
@@ -17,9 +17,16 @@
 <script>
 import "flag-icon-css/css/flag-icon.css";
 import Flag from "../components/Flag";
+import { mapState } from "vuex";
+
 export default {
   name: "country",
-  components: { Flag }
+  components: { Flag },
+  computed: {
+    ...mapState({
+      countries: state => state.countryCodes
+    })
+  }
 };
 </script>
 
