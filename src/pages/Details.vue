3fdfe236c1a48f0dd4ad96a794744654a3b2062d
GILLETTE, ALEX [AG-Contractor/1000] <alex.gillette@monsanto.com>
2018-10-13 12:24:09 -0500

    made styling and adding more stubs to detail page

--- src/pages/Details.vue
+++ src/pages/Details.vue
@@ -28,25 +28,50 @@
       <v-layout row wrap>
         <v-flex xs9/>
         <v-flex xs1>
-          <div :class="'d-flex flag-icon mb-2 flag-icon-us'" xs1/>
+          <div :class="'d-flex flag-icon mb-2 flag-icon-us'"/>
         </v-flex>
         <v-flex xs1>
-          <div :class="'d-flex flag-icon mb-2 flag-icon-tr'" xs1/>
+          <div :class="'d-flex flag-icon mb-2 flag-icon-tr'"/>
         </v-flex>
         <v-flex xs1/>
       </v-layout>
       <v-layout row wrap>
         <v-flex xs4>
-          <v-icon>fas fa-frown</v-icon>
+          <v-icon color="red">fas fa-frown</v-icon>
         </v-flex>
         <v-flex xs4>
-          <v-icon>fas fa-meh</v-icon>
+          <v-icon color="yellow">fas fa-meh</v-icon>
         </v-flex>
         <v-flex xs4>
-          <v-icon>fas fa-smile</v-icon>
+          <v-icon color="green">fas fa-smile</v-icon>
         </v-flex>
       </v-layout>
-      <v-layout row wrap>
+      <v-layout row wrap mt-4>
+        <v-flex xs2>
+          <v-icon color="blue">fas fa-user</v-icon>
+        </v-flex>
+        <v-flex xs7>
+          <v-text-field color="blue" label="Regular"/>
+        </v-flex>
+        <v-flex xs3>
+          <v-btn color="info">Post</v-btn>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap mt-2>
+        <v-flex xs2>
+          <v-icon color="grey">fas fa-user</v-icon>
+        </v-flex>
+        <v-flex xs10>
+          <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare lacus vitae enim tincidunt, a bibendum enim feugiat. Aliquam tincidunt justo eu nunc sollicitudin facilisis.</div>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap mt-2>
+        <v-flex xs2>
+          <v-icon color="grey">fas fa-user</v-icon>
+        </v-flex>
+        <v-flex xs10>
+          <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare lacus vitae enim tincidunt, a bibendum enim feugiat. Aliquam tincidunt justo eu nunc sollicitudin facilisis. Nulla ut elit sollicitudin.</div>
+        </v-flex>
       </v-layout>
     </v-container>
   </div>
