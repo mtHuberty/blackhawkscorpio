1480e75d3f2b2c450b9a6c409889e8bf3ca0f1db
Matt Huberty <matthewhuberty77@gmail.com>
2018-10-13 16:13:37 -0500

    http service working

--- src/store.js
+++ src/store.js
@@ -34,13 +34,13 @@ export default new Vuex.Store({
       new Place('123465234234', 12.0, 12.0, [new Rating('12345689', 'us', 7, [])]),
     ],
     countryCodes: ["ae", "af", "ag", "ai", "al", "am", "ao", "aq", "ar", "as ", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bl", "bm", "bn", "bo", "bq", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "cu", "cv", "cw", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "eh", "er", "es-ct", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb - eng", "gb - nir", "gb - sct", "gb - wls", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in ", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mf", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "ss", "st", "sv", "sx", "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "um", "un", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "ye", "yt", "za", "zm", "zw"],
-    demoCountryCodes: ["us", "tr", "mx", "cn", "in", "id", "jp", "kr", "ng", "pl"],
+    demoCountryCodes: ["us", "tr", "mx", "cn", "in", "tn", "id", "jp", "kr", "th", "ng", "pl"],
     languagesPerCountry: {
       "us": [{ code: "en", text: "English" }],
       "tr": [{ code: "tr", text: "Türk"}, { code: "kmr", text: "کوردیا ژۆرین"}, { code: "ar", text: "عربى" }, { code: "en", text: "English" }],
       "mx": [{ code: "mx", text: "Español"}, { code: "en", text: "English" }],
       "cn": [{ code: "zh-Hans", text: "汉语"}, { code: "mn", text: "монгол хэл" }, { code: "zh", text: "བོད་སྐད།" }, { code: "en", text: "English" }],
-      "in": [{ code: "zh-Hans", text: "汉语"}, { code: "mn", text: "монгол хэл" }, { code: "zh", text: "བོད་སྐད།" }],
+      "in": [{ code: "hi", text: "हिंदी"}, { code: "mn", text: "монгол хэл" }, { code: "zh", text: "བོད་སྐད།" }],
     },
     mapCountryFilter: ''
   },
@@ -53,8 +53,8 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    fetchPlaces({commit, state}) {
-      // return axios.get(`ians url here`)
+    fetchPlaces({commit, state}, serviceType) {
+      return axios.get(`http://localhost:3000/placesSearch/${serviceType}`)
       return new Promise((resolve, reject) => {
         setTimeout(() => {
           resolve(schoolData);
