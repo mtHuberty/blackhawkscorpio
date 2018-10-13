1e55a2d70fc8cc5fdc55195f01c86a23d069de0c
GILLETTE, ALEX [AG-Contractor/1000] <alex.gillette@monsanto.com>
2018-10-13 15:54:54 -0500

    almost done with adding text to toolbar

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
     <v-toolbar v-if="!isHomePage" fixed app flat dark color="primary">
-      <v-btn icon @click="goBack()">
-        <v-icon>arrow_back</v-icon>
-      </v-btn>
+      <v-layout row>
+        <v-flex xs5>
+          <v-btn icon @click="goBack()">
+            <v-icon>arrow_back</v-icon>
+          </v-btn>
+        </v-flex>
+        <v-flex xs7 mt-2>
+          <h1> 
+            {{getToolbarText}}
+          </h1>
+        </v-flex>
+      </v-layout>
     </v-toolbar>
     <v-content>
         <router-view/>
@@ -16,6 +25,19 @@ export default {
   computed: {
     isHomePage: function() {
       return this.$route.fullPath === "/";
+    },
+    getToolbarText: function() {
+      const route = this.$route.fullPath || '';
+      console.log(route)
+
+      if(route.includes('language')){
+        return 'Hizmetler'
+      }
+      if(route.includes('country')){
+        return 'Language'
+      }
+
+      return '';
     }
   },
   methods: {
