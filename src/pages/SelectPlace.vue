54faa08e94a101bd2405050a1dec106ac47e2d9a
Chris Marfia <chris.marfia@monsanto.com>
2018-10-13 10:38:32 -0500

    Add mapping

--- src/pages/SelectPlace.vue
+++ src/pages/SelectPlace.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="place">
-    <h1>This is an place page</h1>
+    <GmapMap :center="center" :zoom="7" style="width: 100%; height: 500px">
+      <GmapMarker v-for="m in markers" :key="m.id" :position="m.position" :clickable="true" :draggable="true"></GmapMarker>
+    </GmapMap>
   </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "select-place",
+  computed: {
+    ...mapState({
+      markers: state => state.places
+    })
+  },
+  data: function() {
+    return {
+      center: {
+        lat: 10.0,
+        lng: 10.0
+      }
+    };
+  }
+};
+</script>
---
+++ src/plugins/maps.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: '',
+        libraries: 'places',
+    }
+    // autobindAllEvents: true,
+    // installComponents: true
+})
