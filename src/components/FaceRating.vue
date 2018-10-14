19bb246ab5dd1283053f2adc6364097cc287b11e
Chris Marfia <chris.marfia@monsanto.com>
2018-10-13 19:19:41 -0500

    Default meh face.

--- src/components/FaceRating.vue
+++ src/components/FaceRating.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <v-icon v-if="rating <= 1.5" color="red" :small="small">fas fa-frown</v-icon>
-    <v-icon v-if="rating > 1.5 && rating < 3.5" color="grey" :small="small">fas fa-meh</v-icon>
-    <v-icon v-if="rating >= 3.5" color="green" :small="small">fas fa-smile</v-icon>
+    <v-icon v-if="rating[0] <= 1.5" color="red" :small="small">fas fa-frown</v-icon>
+    <v-icon v-if="(rating[0] > 1.5 && rating[0] < 3.5)" color="grey" :small="small">fas fa-meh</v-icon>
+    <v-icon v-if="rating[0] === undefined" color="grey" :small="small">fas fa-meh</v-icon>
+    <v-icon v-if="rating [0]>= 3.5" color="green" :small="small">fas fa-smile</v-icon>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FaceRating',
-  props: ['rating', 'small']
-}
+  name: "FaceRating",
+  props: ["rating", "small"]
+};
 </script>
